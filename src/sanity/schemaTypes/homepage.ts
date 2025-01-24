b233f575f82807f12b90d41af0727529388e8899
renskiedulog <22104607@usc.edu.ph>
2025-01-24 17:15:28 +0800

    Added: A Singleton Document for Homepage

---
+++ src/sanity/schemaTypes/homepage.ts
@@ -0,0 +1,17 @@
+export const homepage = {
+  name: "homepage",
+  title: "Home Page",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+  ],
+};
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,15 +1,27 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title("Blog")
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
+      S.documentTypeListItem("post").title("Posts"),
+      S.documentTypeListItem("category").title("Categories"),
+      S.documentTypeListItem("author").title("Authors"),
       S.divider(),
+      S.listItem()
+        .title("Home Page")
+        .child(
+          S.editor()
+            .id("homepage")
+            .schemaType("homepage")
+            .documentId("homepage")
+        ),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
+        (item) =>
+          item.getId() &&
+          !["post", "category", "author", "project", "homepage"].includes(
+            item.getId()!
+          )
       ),
-    ])
+    ]);
