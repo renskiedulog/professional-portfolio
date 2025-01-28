4373e1ff5792b5023c2c43d29bcc9cb75abbfd83
renskiedulog <22104607@usc.edu.ph>
2025-01-28 20:43:59 +0800

    Added: Blog Cards

---
+++ src/app/(pages)/blog/blog-card.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
+interface Blog {
+  image: string;
+  title: string;
+  description: string;
+  date: string;
+  author: string;
+  categories: string[];
+  link: string;
+}
+
+const BlogCard = ({ blog }: { blog: Blog }) => {
+  return (
+    <article className="border rounded-md overflow-hidden shadow-sm bg-background h-max mb-4">
+      <Image
+        className="aspect-video object-cover"
+        src={blog.image}
+        width={800}
+        height={800}
+        alt={blog.title}
+      />
+      <div className="px-3.5 py-3.5">
+        <h3 className="font-bold text-lg leading-tight">{blog.title}</h3>
+        <p className="font-medium text-sm mt-2">
+          {blog.description.length > 150
+            ? `${blog.description.substring(0, 150)}...`
+            : blog.description}
+        </p>
+        <div className="mt-2 flex gap-1 flex-wrap">
+          {blog.categories.map((category, index) => (
+            <span
+              key={index}
+              className="bg-primary-foreground dark:bg-white/20 text-primary text-[11px] py-0.5 px-1.5 border border-opacity-10 rounded text-nowrap font-semibold"
+            >
+              {category}
+            </span>
+          ))}
+        </div>
+        <Separator className="mt-2" />
+        <div className="mt-2 flex justify-between items-center">
+          <p className="text-xs text-muted-foreground mt-1">{blog.date}</p>
+          <Link href={blog.link} target="_blank">
+            <Button className="text-xs" variant="default" size="sm">
+              Read More
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default BlogCard;
--- src/app/(pages)/blog/page.tsx
+++ src/app/(pages)/blog/page.tsx
@@ -5,26 +5,68 @@ import BlurFade from "@/app/UI/animation-wrappers/fade";
 import { List, Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { IoGridOutline } from "react-icons/io5";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import BlogCard from "./blog-card";
+import { useStorage } from "@/app/UI/global-components/storage-provider";
+
+const blogs = [
+  {
+    image: "/projects/portfolio.webp",
+    title: "Mastering React: A Complete Guide",
+    description:
+      "Learn everything about React, from the basics to advanced concepts, and become a proficient developer.",
+    date: "2025-01-01",
+    author: "John Doe",
+    categories: ["React", "JavaScript", "Web Development"],
+    link: "https://example.com/react-guide",
+  },
+  {
+    image: "/projects/portfolio.webp",
+    title: "Top 10 Tips for Next.js Developers",
+    description:
+      "Enhance your Next.js projects with these practical tips and best practices for optimized performance and scalability.",
+    date: "2025-01-15",
+    author: "Alice Johnson",
+    categories: ["Next.js", "Web Development", "Performance"],
+    link: "https://example.com/nextjs-tips",
+  },
+  {
+    image: "/projects/portfolio.webp",
+    title: "CSS Tricks You Should Know",
+    description:
+      "A collection of useful CSS tricks and techniques to elevate your web design skills.",
+    date: "2025-01-20",
+    author: "Michael Brown",
+    categories: ["CSS", "Design", "Frontend"],
+    link: "https://example.com/css-tricks",
+  },
+];
 
 const page = () => {
+  const { getItem, setItem } = useStorage();
   const [layout, setLayout] = useState("grid");
 
   const toggleLayout = () => {
     setLayout((prev) => (prev === "grid" ? "list" : "grid"));
+    setItem("blog_orientation", layout === "grid" ? "list" : "grid");
   };
+
+  useEffect(() => {
+    const initLayout = getItem("blog_orientation");
+    setLayout((initLayout as string) ?? "grid");
+  }, []);
 
   return (
     <Container>
-      <BlurFade>
-        <div className="w-full flex justify-between px-3 sm:px-5">
+      <BlurFade className="px-3 sm:px-5">
+        <div className="w-full flex justify-between">
           <BackButton href="/" />
           {/* Filter Options */}
           <div className="flex items-center gap-3">
             <div className="relative">
               <Input
                 type="text"
-                className="h-max w-[150px] bg-white dark:bg-primary/10 sm:text-base text-sm pl-8 peer focus:w-[250px] sm:focus:w-[300px] transition-all duration-200 ease-in-out"
+                className="h-max w-[120px] sm:w-[150px] bg-white dark:bg-primary/10 sm:text-base text-sm pl-8 peer focus:w-[250px] sm:focus:w-[300px] transition-all duration-200 ease-in-out"
                 placeholder="Search..."
               />
               <Search
@@ -33,24 +75,45 @@ const page = () => {
               />
             </div>
             {layout === "grid" ? (
-              <BlurFade key="grid" duration={0.1} yOffset={0}>
-                <IoGridOutline
-                  size={20}
-                  className="opacity-70 cursor-pointer hover:scale-110 transition duration-200 ease-in-out"
-                  onClick={toggleLayout}
-                />
-              </BlurFade>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleLayout();
+                }}
+              >
+                <BlurFade key="grid" duration={0.1} yOffset={0}>
+                  <IoGridOutline
+                    size={20}
+                    className="opacity-70 cursor-pointer hover:scale-110 transition duration-200 ease-in-out"
+                  />
+                </BlurFade>
+              </button>
             ) : (
-              <BlurFade key="list" duration={0.1} yOffset={0}>
-                <List
-                  size={20}
-                  className="opacity-70 cursor-pointer hover:scale-110 transition duration-200 ease-in-out"
-                  onClick={toggleLayout}
-                />
-              </BlurFade>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleLayout();
+                }}
+              >
+                <BlurFade key="list" duration={0.1} yOffset={0}>
+                  <List
+                    size={20}
+                    className="opacity-70 cursor-pointer hover:scale-110 transition duration-200 ease-in-out"
+                  />
+                </BlurFade>
+              </button>
             )}
           </div>
         </div>
+        <BlurFade key={layout}>
+          <div
+            className={`${layout === "grid" ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3" : "flex flex-col"} gap-5 py-5`}
+          >
+            {blogs.map((blog, idx) => (
+              <BlogCard blog={blog} key={idx} />
+            ))}
+          </div>
+        </BlurFade>
       </BlurFade>
     </Container>
   );
