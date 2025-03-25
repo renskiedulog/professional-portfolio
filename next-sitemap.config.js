f831e625269e928db5521b81c325e7b80342b68d
renskiedulog <22104607@usc.edu.ph>
2025-03-25 11:53:17 +0800

    Fix: Sitemap Issues

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://renato-dulog.vercel.app/", // Update with your domain
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL || "https://renato-dulog.vercel.app/", // Update with your domain
   generateRobotsTxt: true,
   sitemapSize: 5000,
-  exclude: ["/studio/*"], // Exclude specific routes
+  exclude: ["/studio/*"],
   robotsTxtOptions: {
     policies: [
       { userAgent: "*", allow: "/" },
