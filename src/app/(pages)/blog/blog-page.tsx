687e63577032f69d1940df9d24286fdf4306f21d
renskiedulog <22104607@usc.edu.ph>
2025-04-29 09:14:23 +0800

    Removed: Searchbar

--- src/app/(pages)/blog/blog-page.tsx
+++ src/app/(pages)/blog/blog-page.tsx
@@ -34,7 +34,7 @@ const BlogPage = ({ blogs, filters }: { blogs: Blog[]; filters: string[] }) => {
           <BackButton href="/" />
           {/* Filter Options */}
           <div className="flex items-center gap-3">
-            <div className="relative">
+            {/* <div className="relative">
               <Input
                 type="text"
                 className="h-max w-[120px] sm:w-[150px] bg-white dark:bg-primary/10 sm:text-base text-sm pl-8 peer focus:w-[180px] sm:focus:w-[300px] transition-all duration-200 ease-in-out"
@@ -48,7 +48,7 @@ const BlogPage = ({ blogs, filters }: { blogs: Blog[]; filters: string[] }) => {
                 size={18}
                 className="absolute top-1/2 left-2 -translate-y-1/2 text-gray-400 opacity-50 transition-opacity duration-300 ease-in-out peer-focus:opacity-100"
               />
-            </div>
+            </div> */}
             <button
               className="items-center gap-2"
               onClick={() => {
